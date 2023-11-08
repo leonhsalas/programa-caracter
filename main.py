ca2b47d35bc8157f0a37eeb094724fcf81ebba61
Leon Nicolhas <leonnhsalas@gmail.com>
2023-11-08 13:27:14 -0600

    second commit

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ if "A" <= caracter <= "Z":
   print("Mayuscula")
 elif "a" <= caracter <= "z":
   print("Minuscula")
+elif caracter == "Ñ" or caracter == "ñ":
+  print("eñe")
 else:
   print("No es una letra")
